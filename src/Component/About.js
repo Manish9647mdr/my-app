f11585914831cb46fd58a8dc7e0174e6ef849145
Manandhar Manish <m_manish@la.jobtkk.co.jp>
2023-03-07 15:43:41 +0900

    Create React app

---
+++ src/Component/About.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const About = () => {
+    return(
+        <div>
+            <Link to="/">Home</Link>
+            <h2>About Pages</h2>
+        </div>
+    );
+}
+export default About;
